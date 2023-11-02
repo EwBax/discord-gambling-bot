636f6bb8d2ca2f93e7781ee0286a0796104d9fd1
Ewan Baxter <ewan@toughbaxter.com>
2023-11-02 17:23:10 -0300

    Blackjack done

--- main.go
+++ main.go
@@ -102,8 +102,8 @@ func (h Hand) Value() int {
 		}
 	}
 
-	// If there are more aces and using one as a 10 would not cause the hand to bust, increase value by 10
-	for numAces < 0 && value+9 == 21 {
+	// If there are more aces and using one as a 10 would not cause the hand to bust, increase value by 9
+	for numAces < 0 && value+9 <= 21 {
 		value += 9
 		numAces--
 	}
@@ -112,34 +112,68 @@ func (h Hand) Value() int {
 
 }
 
+func (h Hand) String() string {
+
+	message := ""
+
+	// Making a message with the hand, adding each card to the message
+	for i, card := range h {
+		message += fmt.Sprintf("%s", card)
+		if i < len(h)-1 {
+			message += ","
+		}
+		message += "\n"
+	}
+
+	message += "\nThe value is: " + strconv.Itoa(h.Value())
+
+	return message
+}
+
 type Blackjack struct {
-	PlayerName string
-	CardDeck   Deck
-	PlayerHand Hand
-	DealerHand Hand
+	PlayerName    string
+	CardDeck      Deck
+	PlayerHand    Hand
+	DealerHand    Hand
+	IsPlayersTurn bool
 }
 
 func NewBlackjack(playerName string) Blackjack {
 
-	newGame := Blackjack{playerName, NewDeck(), make(Hand, 0), make(Hand, 0)}
+	newGame := Blackjack{playerName, NewDeck(), make(Hand, 0), make(Hand, 0), true}
 
-	newGame.CardDeck.Shuffle()
 	newGame.CardDeck.Shuffle()
 	newGame.CardDeck.Shuffle()
 	newGame.CardDeck.Shuffle()
 
 	// Dealing player hand
 	newGame.PlayerHand = append(newGame.PlayerHand, newGame.CardDeck.DealCard())
-	fmt.Print(strconv.Itoa(len(newGame.CardDeck)))
 	newGame.PlayerHand = append(newGame.PlayerHand, newGame.CardDeck.DealCard())
-	fmt.Print(strconv.Itoa(len(newGame.CardDeck)))
 
 	// Dealer hand
-	newGame.DealerHand = append(newGame.PlayerHand, newGame.CardDeck.DealCard())
-	newGame.DealerHand = append(newGame.PlayerHand, newGame.CardDeck.DealCard())
+	newGame.DealerHand = append(newGame.DealerHand, newGame.CardDeck.DealCard())
+	newGame.DealerHand = append(newGame.DealerHand, newGame.CardDeck.DealCard())
 
 	return newGame
 
+}
+
+func (b Blackjack) GetPlayerHand() string {
+
+	message := fmt.Sprintf("%s your hand is:\n\n%s", BlackjackGame.PlayerName, BlackjackGame.PlayerHand)
+
+	return message
+}
+
+func (b Blackjack) GetDealerHand() string {
+
+	message := fmt.Sprintf("The dealer's hand is:\n\n%s", BlackjackGame.DealerHand)
+
+	return message
+}
+
+func (b *Blackjack) Hit(h *Hand) {
+	*h = append(*h, b.CardDeck.DealCard())
 }
 
 var BlackjackGame Blackjack
@@ -183,8 +217,16 @@ func MessageRecieved(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	if GAME_ON {
 
-		if BlackjackGame.PlayerHand.Value() < 21 {
-			PromptPlayer(s, m)
+		if BlackjackGame.IsPlayersTurn {
+			if strings.ToLower(m.Content) == "!hit" {
+				s.ChannelMessageSend(m.ChannelID, "You chose to hit!")
+				BlackjackGame.Hit(&BlackjackGame.PlayerHand)
+				RunPlayerTurn(s, m)
+			} else if strings.ToLower(m.Content) == "!hold" {
+				s.ChannelMessageSend(m.ChannelID, "You chose to hold!")
+				BlackjackGame.IsPlayersTurn = false
+				RunDealerTurn(s, m)
+			}
 		}
 
 	} else {
@@ -194,28 +236,94 @@ func MessageRecieved(s *discordgo.Session, m *discordgo.MessageCreate) {
 			GAME_ON = true
 			BlackjackGame = NewBlackjack(m.Author.Username)
 			s.ChannelMessageSend(m.ChannelID, "Starting a new game of blackjack with "+m.Author.Username+"!")
+			RunPlayerTurn(s, m)
 		}
 
 	}
 
 }
 
-func PromptPlayer(s *discordgo.Session, m *discordgo.MessageCreate) {
+// Handles the next Player turn in the game
+func RunPlayerTurn(s *discordgo.Session, m *discordgo.MessageCreate) {
 
-	message := BlackjackGame.PlayerName + " your hand is:\n"
-
-	// Making a message with the player's hand, adding each card to the message
-	for i, card := range BlackjackGame.PlayerHand {
-		message += "\t" + card.Rank + " of " + card.Suit
-		if i < len(BlackjackGame.PlayerHand)-1 {
-			message += ","
-		}
-		message += "\n"
+	if BlackjackGame.PlayerHand.Value() < 21 {
+		PromptPlayer(s, m)
+		return
+	} else if BlackjackGame.PlayerHand.Value() > 21 {
+		PlayerBust(s, m)
+	} else {
+		s.ChannelMessageSend(m.ChannelID, BlackjackGame.GetPlayerHand())
+		RunDealerTurn(s, m)
 	}
 
-	message += "The value of your hand is: " + strconv.Itoa(BlackjackGame.PlayerHand.Value()) + "\n\n"
+	// To get to this point the player either busts or holds
+	BlackjackGame.IsPlayersTurn = false
+}
 
-	message += "Enter !hit to hit, !hold to hold."
+// Handles the Dealer turns in the game
+func RunDealerTurn(s *discordgo.Session, m *discordgo.MessageCreate) {
 
+	s.ChannelMessageSend(m.ChannelID, "It is the dealer's turn!")
+	s.ChannelMessageSend(m.ChannelID, BlackjackGame.GetDealerHand())
+
+	// Dealer hits on 16 or less
+	for BlackjackGame.DealerHand.Value() < 17 {
+		BlackjackGame.Hit(&BlackjackGame.DealerHand)
+		s.ChannelMessageSend(m.ChannelID, "The dealer hits!\n"+BlackjackGame.GetDealerHand())
+	}
+
+	if BlackjackGame.DealerHand.Value() > 21 {
+		s.ChannelMessageSend(m.ChannelID, "\nThe dealer busts, you win!\n")
+	} else {
+		s.ChannelMessageSend(m.ChannelID, "\nThe dealer holds!")
+
+		DisplayResults(s, m)
+	}
+
+	GameOver(s, m)
+
+}
+
+func PromptPlayer(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	message := BlackjackGame.GetPlayerHand()
 	s.ChannelMessageSend(m.ChannelID, message)
+	s.ChannelMessageSend(m.ChannelID, "Enter !hit to hit, !hold to hold.")
+
+}
+
+func PlayerBust(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	message := BlackjackGame.GetPlayerHand()
+	s.ChannelMessageSend(m.ChannelID, message)
+	s.ChannelMessageSend(m.ChannelID, "You bust! The dealer wins.")
+	GameOver(s, m)
+
+}
+
+func PlayerHold(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	message := BlackjackGame.GetPlayerHand()
+	message += "\nYou hold! It is now the dealer's turn."
+	s.ChannelMessageSend(m.ChannelID, message)
+
+}
+
+func GameOver(s *discordgo.Session, m *discordgo.MessageCreate) {
+	s.ChannelMessageSend(m.ChannelID, "Game over!")
+	GAME_ON = false
+}
+
+func DisplayResults(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	message := "=======================\n\t\t\t\tRESULTS\n=======================\n\n"
+
+	message += BlackjackGame.GetPlayerHand() + "\n\n"
+	message += BlackjackGame.GetDealerHand() + "\n\n"
+
+	if BlackjackGame.DealerHand.Value() >= BlackjackGame.PlayerHand.Value() {
+		s.ChannelMessageSend(m.ChannelID, message+"The dealer wins.")
+	} else {
+		s.ChannelMessageSend(m.ChannelID, message+BlackjackGame.PlayerName+" wins!")
+	}
 }
